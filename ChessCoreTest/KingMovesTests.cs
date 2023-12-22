bd52c2d9a7b076a533e44e5bb3ccc1487dc0a507
Nick Darlow <dangerous.darlow@gmail.com>
2023-12-22 09:15:09 +0000

    Add test verifying castling can be blocked by piece position
    Also CollectionAssert.IsSubsetOf and CollectionAssert.IsNotSubsetOf don't work as you might expect so I stopped using them

--- ChessCoreTest/KingMovesTests.cs
+++ ChessCoreTest/KingMovesTests.cs
@@ -60,16 +60,12 @@ public class KingMovesTests : MovesTests
         var board = Board.CreateFromForsythEdwardsNotation("rnbqk2r/pppp1ppp/3b1n2/4p3/4P3/3B1N2/PPPP1PPP/RNBQK2R w KQkq - 4 4");
         var moves = MoveCalculator.GetMovesForColour(board, Colour.White);
 
-        CollectionAssert.IsSubsetOf(new List<Move>
-        {
-            new(e1, g1, MoveType.Castle)
-        }, moves, "King side castle is not present in move list but is possible");
+        CollectionAssert.Contains(moves, new Move(e1, g1, MoveType.Castle),
+            "King side castle is not present in move list but is possible");
 
-        CollectionAssert.IsNotSubsetOf(new List<Move>
-        {
-            new(e1, c1, MoveType.Castle)
-        }, moves, "Queen side castle is present in move list but is not possible");
-        
+        CollectionAssert.DoesNotContain(moves, new Move(e1, c1, MoveType.Castle),
+            "Queen side castle is present in move list but is not possible");
+
         board.ApplyMove(new Move(e1, g1, MoveType.Castle));
         Assert.Multiple(() =>
         {
@@ -81,15 +77,11 @@ public class KingMovesTests : MovesTests
 
         moves = MoveCalculator.GetMovesForColour(board, Colour.Black);
 
-        CollectionAssert.IsSubsetOf(new List<Move>
-        {
-            new(e8, g8, MoveType.Castle)
-        }, moves, "Black queen side castle is not present in move list but is possible");
+        CollectionAssert.Contains(moves, new Move(e8, g8, MoveType.Castle),
+            "King side castle is not present in move list but is possible");
 
-        CollectionAssert.IsNotSubsetOf(new List<Move>
-        {
-            new(e8, c8, MoveType.Castle)
-        }, moves, "Black king side castle is present in move list but is not possible");
+        CollectionAssert.DoesNotContain(moves, new Move(e8, c8, MoveType.Castle),
+            "Queen side castle is present in move list but is not possible");
 
         board.ApplyMove(new Move(e8, g8, MoveType.Castle));
         Assert.Multiple(() =>
@@ -107,15 +99,11 @@ public class KingMovesTests : MovesTests
         var board = Board.CreateFromForsythEdwardsNotation("r3kbnr/p1ppqppp/bpn5/4p3/4P3/BPN5/P1PPQPPP/R3KBNR w KQkq - 2 6");
         var moves = MoveCalculator.GetMovesForColour(board, Colour.White);
 
-        CollectionAssert.IsSubsetOf(new List<Move>
-        {
-            new(e1, c1, MoveType.Castle)
-        }, moves, "White queen side castle is not present in move list but is possible");
+        CollectionAssert.Contains(moves, new Move(e1, c1, MoveType.Castle),
+            "Queen side castle is not present in move list but is possible");
 
-        CollectionAssert.IsNotSubsetOf(new List<Move>
-        {
-            new(e1, g1, MoveType.Castle)
-        }, moves, "White king side castle is present in move list but is not possible");
+        CollectionAssert.DoesNotContain(moves, new Move(e1, g1, MoveType.Castle),
+            "King side castle is present in move list but is not possible");
 
         board.ApplyMove(new Move(e1, c1, MoveType.Castle));
         Assert.Multiple(() =>
@@ -128,15 +116,11 @@ public class KingMovesTests : MovesTests
 
         moves = MoveCalculator.GetMovesForColour(board, Colour.Black);
 
-        CollectionAssert.IsSubsetOf(new List<Move>
-        {
-            new(e8, c8, MoveType.Castle)
-        }, moves, "Black queen side castle is not present in move list but is possible");
+        CollectionAssert.Contains(moves, new Move(e8, c8, MoveType.Castle),
+            "Queen side castle is not present in move list but is possible");
 
-        CollectionAssert.IsNotSubsetOf(new List<Move>
-        {
-            new(e8, g8, MoveType.Castle)
-        }, moves, "Black king side castle is present in move list but is not possible");
+        CollectionAssert.DoesNotContain(moves, new Move(e8, g8, MoveType.Castle),
+            "King side castle is present in move list but is not possible");
 
         board.ApplyMove(new Move(e8, c8, MoveType.Castle));
         Assert.Multiple(() =>
@@ -146,5 +130,54 @@ public class KingMovesTests : MovesTests
             Assert.That(board.IsBlackKingsideCastleAvailable, Is.False, "Black king side castle is still available");
             Assert.That(board.IsBlackQueensideCastleAvailable, Is.False, "Black queen side castle is still available");
         });
+    }
+
+    [Test]
+    public void Castling_is_not_possible_if_blocked_by_pieces()
+    {
+        var board = Board.CreateFromForsythEdwardsNotation("rnbqk1nr/ppppbppp/8/4p3/4P3/8/PPPPBPPP/RNBQK1NR w KQkq - 2 3");
+        var moves = MoveCalculator.GetMovesForColour(board, Colour.White);
+        moves.AddRange(MoveCalculator.GetMovesForColour(board, Colour.Black));
+
+        CollectionAssert.DoesNotContain(moves, new Move(e1, g1, MoveType.Castle));
+        CollectionAssert.DoesNotContain(moves, new Move(e1, c1, MoveType.Castle));
+        CollectionAssert.DoesNotContain(moves, new Move(e8, g8, MoveType.Castle));
+        CollectionAssert.DoesNotContain(moves, new Move(e8, c8, MoveType.Castle));
+
+        board = Board.CreateFromForsythEdwardsNotation("rnbqkb1r/pppp1ppp/5n2/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R w KQkq - 2 3");
+        moves = MoveCalculator.GetMovesForColour(board, Colour.White);
+        moves.AddRange(MoveCalculator.GetMovesForColour(board, Colour.Black));
+
+        CollectionAssert.DoesNotContain(moves, new Move(e1, g1, MoveType.Castle));
+        CollectionAssert.DoesNotContain(moves, new Move(e1, c1, MoveType.Castle));
+        CollectionAssert.DoesNotContain(moves, new Move(e8, g8, MoveType.Castle));
+        CollectionAssert.DoesNotContain(moves, new Move(e8, c8, MoveType.Castle));
+
+        board = Board.CreateFromForsythEdwardsNotation("rn2kbnr/pppbpppp/3q4/3p4/3P4/3Q4/PPPBPPPP/RN2KBNR w KQkq - 0 1");
+        moves = MoveCalculator.GetMovesForColour(board, Colour.White);
+        moves.AddRange(MoveCalculator.GetMovesForColour(board, Colour.Black));
+
+        CollectionAssert.DoesNotContain(moves, new Move(e1, g1, MoveType.Castle));
+        CollectionAssert.DoesNotContain(moves, new Move(e1, c1, MoveType.Castle));
+        CollectionAssert.DoesNotContain(moves, new Move(e8, g8, MoveType.Castle));
+        CollectionAssert.DoesNotContain(moves, new Move(e8, c8, MoveType.Castle));
+
+        board = Board.CreateFromForsythEdwardsNotation("r1b1kbnr/ppp1pppp/2nq4/3p4/3P4/2NQ4/PPP1PPPP/R1B1KBNR w KQkq - 0 1");
+        moves = MoveCalculator.GetMovesForColour(board, Colour.White);
+        moves.AddRange(MoveCalculator.GetMovesForColour(board, Colour.Black));
+
+        CollectionAssert.DoesNotContain(moves, new Move(e1, g1, MoveType.Castle));
+        CollectionAssert.DoesNotContain(moves, new Move(e1, c1, MoveType.Castle));
+        CollectionAssert.DoesNotContain(moves, new Move(e8, g8, MoveType.Castle));
+        CollectionAssert.DoesNotContain(moves, new Move(e8, c8, MoveType.Castle));
+
+        board = Board.CreateFromForsythEdwardsNotation("r2qkbnr/pppbpppp/2n5/3p4/3P4/2N5/PPPBPPPP/R2QKBNR w KQkq - 0 1");
+        moves = MoveCalculator.GetMovesForColour(board, Colour.White);
+        moves.AddRange(MoveCalculator.GetMovesForColour(board, Colour.Black));
+
+        CollectionAssert.DoesNotContain(moves, new Move(e1, g1, MoveType.Castle));
+        CollectionAssert.DoesNotContain(moves, new Move(e1, c1, MoveType.Castle));
+        CollectionAssert.DoesNotContain(moves, new Move(e8, g8, MoveType.Castle));
+        CollectionAssert.DoesNotContain(moves, new Move(e8, c8, MoveType.Castle));
     }
 }
